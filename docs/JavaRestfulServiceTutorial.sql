b514e203f3f37dc6b5e60fa2a28958ed20e79406
Paul Milazzo <paul.j.milazzo@accenture.com>
2014-08-26 21:29:57 -0400

    Added new sql script to drop and recreate PC_PARTS table with incrementing primary key and non-null varchars

--- docs/JavaRestfulServiceTutorial.sql
+++ docs/JavaRestfulServiceTutorial.sql
@@ -1,7 +1,22 @@
-CREATE TABLE PC_PARTS(PC_PARTS_PK INT PRIMARY KEY, PC_PARTS_TITLE VARCHAR(30), PC_PARTS_CODE INT, PC_PARTS_MAKER VARCHAR(30), PC_PARTS_AVAIL INT, PC_PARTS_DESC VARCHAR(100));
-INSERT INTO PC_PARTS VALUES(1, 'Intel MB', 123456789, 'Intel', 5, 'Intel motherboard');
-INSERT INTO PC_PARTS VALUES(2, 'AMD MB', 987654321, 'AMD', 3, 'AMD motherboard');
-INSERT INTO PC_PARTS VALUES(3, 'HP Laptop', 123456, 'HP', 5, 'HP Laptop with 15 inch screen');
-INSERT INTO PC_PARTS VALUES(4, 'IBM Desktop', 123457, 'IBM', 10, 'IBM Desktop');
-INSERT INTO PC_PARTS VALUES(5, 'Dell Desktop', 123458, 'DELL', 14, 'Dell Desktop');
-INSERT INTO PC_PARTS VALUES(6, 'HP Desktop', 123459, 'HP', 3, 'HP Desktop');
+DROP TABLE PC_PARTS;
+
+CREATE TABLE PC_PARTS
+(
+pc_parts_pk INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+pc_parts_title VARCHAR(30) NOT NULL,
+pc_parts_code INTEGER NOT NULL,
+pc_parts_maker VARCHAR(30) NOT NULL,
+pc_parts_avail INTEGER NOT NULL,
+pc_parts_desc VARCHAR(100) NOT NULL,
+CONSTRAINT primary_key PRIMARY KEY (pc_parts_pk)
+) ;
+
+INSERT INTO PC_PARTS(pc_parts_title, pc_parts_code, pc_parts_maker, pc_parts_avail, pc_parts_desc) values('Intel MB', 123456789, 'Intel', 5, 'Intel motherboard');
+INSERT INTO PC_PARTS(pc_parts_title, pc_parts_code, pc_parts_maker, pc_parts_avail, pc_parts_desc) values('AMD MB', 987654321, 'AMD', 3, 'AMD motherboard');
+INSERT INTO PC_PARTS(pc_parts_title, pc_parts_code, pc_parts_maker, pc_parts_avail, pc_parts_desc) values('HP Laptop', 123456, 'HP', 5, 'HP Laptop with 15 inch screen');
+INSERT INTO PC_PARTS(pc_parts_title, pc_parts_code, pc_parts_maker, pc_parts_avail, pc_parts_desc) values('IBM Desktop', 123457, 'IBM', 10, 'IBM Desktop');
+INSERT INTO PC_PARTS(pc_parts_title, pc_parts_code, pc_parts_maker, pc_parts_avail, pc_parts_desc) values('Dell Desktop', 123458, 'DELL', 14, 'Dell Desktop');
+INSERT INTO PC_PARTS(pc_parts_title, pc_parts_code, pc_parts_maker, pc_parts_avail, pc_parts_desc) values('HP Desktop', 123459, 'HP', 3, 'HP Desktop');
+
+
+
